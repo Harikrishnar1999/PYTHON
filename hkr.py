f3384a81da2b8d54fb21ca98fdcf070e2b7753da
Harikrishnar1999 <116347432+Harikrishnar1999@users.noreply.github.com>
2022-11-02 14:49:25 +0530

    Add files via upload

---
+++ hkr.py
@@ -0,0 +1,10 @@
+a=int(input("enter a value"))
+b=int(input("enter your second value"))
+c=int(input("enter your third value"))
+if a>b:
+    print(a,"is greater")
+elif b>c:
+    print(b,"is greater")
+else:
+    print(c,"is greater")
+   
