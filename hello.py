d8839f3d370df653fe229c794881ab34558b1f34
Harikrishnar1999 <116347432+Harikrishnar1999@users.noreply.github.com>
2022-11-02 14:50:38 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+a=int(input("enter a value"))
+print("your number is ",a)
+b=int(input("enter another number"))
+print("your second number is",b)
+if a>b:
+    print(a,"is greater than",b)
+else:
+    print(b,"is greater than",a)
