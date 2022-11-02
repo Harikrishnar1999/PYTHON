d65243836d15507ba6697fca123395c5f812c553
Harikrishnar1999 <116347432+Harikrishnar1999@users.noreply.github.com>
2022-11-02 14:45:37 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,9 @@
+dict1={'name':'hari','age':'23','sex':'male','address':'harinivas alakkottu'}
+print(dict1)
+dict2=dict1.copy()
+print(dict2)
+print("value = ",dict1.values())
+print("key =",dict1.values())
+dict3={'aadhar': 78578586754574}
+dict1.update(dict3)
+print(dict1)
